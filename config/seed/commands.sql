1d6a586fa2e4942bb96b40fef5fd99f298e08cca
Lewis Dukelow <35412216+m2a9x45@users.noreply.github.com>
2024-08-07 19:12:58 +0100

    Update migrations

--- config/seed/commands.sql
+++ config/seed/commands.sql
@@ -1,14 +1,14 @@
 INSERT INTO `commands` (path_id, command, workflow_id) VALUES
 ('open_banking', 'open-banking', 'connected-accounts'),
-('easy_tranfer', 'easy-tranfer', 'easy-tranfers');
-('test_test', 'wf_test', 'wf-test');
+('easy_tranfer', 'easy-tranfer', 'easy-tranfers'),
+('test_test', 'wf_test', 'wf-test'),
 ('jam_jam', 'wf_test_2', 'wf-jam');
 
-INSERT INTO `workflows` VALUES 
-('wf-test','test_test')
+INSERT INTO `workflows` (workflow_id, trigger_id) VALUES 
+('wf-test','test_test'),
 ('wf-jam','jam_jam');
 
-INSERT INTO `wf_steps` VALUES 
+INSERT INTO `wf_steps` (wf_id, step_id, action, message) VALUES 
 ('wf-test','wf-test_step_1','button_selection','Step 1: What is the issue?'),
 ('wf-test','wf-test_step_2','button_selection','Step 2: Choose consent approval option'),
 ('wf-test','wf-test_step_3','send_message','Step 3: Workflow finished'),
@@ -16,7 +16,7 @@ INSERT INTO `wf_steps` VALUES
 ('wf-jam','wf-jam_step_2','open_modal','test'),
 ('wf-jam','wf-jam_step_3','send_message','Step 4: Workflow finished');
 
-INSERT INTO `wf_branches` VALUES 
+INSERT INTO `wf_branches` (step_id, action_id, next_step_id, text) VALUES 
 ('wf-test_step_1','wf-test_button-click-1','wf-test_step_2','Consent approval screen'),
 ('wf-test_step_1','wf-test_button-click-2','wf-test_step_3','Easy Transfer'),
 ('wf-test_step_2','wf-test_button-click-3','wf-test_step_4','Button 1'),
@@ -25,7 +25,7 @@ INSERT INTO `wf_branches` VALUES
 ('wf-jam_step_1','wf-jam_button-click-1','wf-jam_step_2','yes'),
 ('wf-jam_step_2','','wf-jam_step_3','test');
 
-INSERT INTO `wf_options` VALUES 
+INSERT INTO `wf_options` (step_id, name, value) VALUES 
 ('wf-jam_step_2','HSBC','hsbc'),
 ('wf-jam_step_2','Lloyds','lloyds'),
 ('wf-jam_step_2','TSB','tsb');
