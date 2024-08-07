bdad966ba4e8a59a46cfd20c7e745602c88aa149
Lewis Dukelow <35412216+m2a9x45@users.noreply.github.com>
2024-08-07 19:27:32 +0100

    Update db config

--- dao/db.py
+++ dao/db.py
@@ -5,12 +5,13 @@ host = os.getenv('DB_HOST')
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
 db_name = os.getenv('DB_NAME')
+db_port = os.getenv('DB_PORT')
 
 
 def get_connection():
     con = mysql.connector.connect(
         host=host,
-        port="4000",
+        port=db_port,
         user=user,
         password=password,
         database=db_name
